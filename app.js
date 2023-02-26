461c1ef3a6e5092850b2421b92a0bd3fc6e63b21
Danish Pathan <animestaan@gmail.com>
2023-02-26 22:22:37 +0530

    FinalEdit

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
 let yourName = document.getElementById('male');
 let partnerName = document.getElementById('female')
 let parinaam = document.getElementById('parinaam');
-let lovePercentage =()=> {return Math.floor(Math.random() *100)};
+let lovePercentage =()=> {return Math.floor(Math.random() *50 +50 )};
 let resultDiv = document.getElementById('results')
 // console.log(parinaam);
 let yourNameValue =0;
 let partnerNameValue = 0;
+let love = 0;
 
 const generatePercentage =()=> {
  yourNameValue =Number(yourName.value.length);
     partnerNameValue = Number(partnerName.value.length);
  
+    love = lovePercentage();
 
 
 }
@@ -19,7 +21,6 @@ const lovePer = () => {
 
     if(yourNameValue>0 && partnerNameValue>0){
 
-        let love = lovePercentage();
         resultDiv.innerHTML = `<h2>Results:  ${love} %</h2>`;
   
     }
